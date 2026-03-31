07f9e8e14ff9882243470a3634b06af4e6760e9b
NoorByte IT Team <noorshifthq@zohomail.in>
2026-04-01 00:19:01 +0530

    download from internet

--- engine/logic/ai/ai_config.js
+++ engine/logic/ai/ai_config.js
@@ -1,13 +1,14 @@
 // Model Identifiers
 export const primaryModelId = "Qwen2.5-0.5B-Instruct-q4f16_1-MLC"; // Qwen2.5-0.5B as the primary LLM
-export const secondaryModelId = "Llama-3.2-1B-Instruct-q4f16_1-MLC"; // Llama-3.2 as the secondary LLM
+export const secondaryModelId = "Llama-3.2-1B-Instruct-q4f32_1-MLC"; // Llama-3.2 as the secondary LLM
 
 // Local Model Configuration
 export const localAppConfig = {
     "model_list": [
         {
-            "model_url": `engine/core/${primaryModelId}`,
-            "local_id": `${primaryModelId}-local`,
+           // "model_url": `engine/core/${primaryModelId}`,
+          //  "local_id": `${primaryModelId}-local`,
+            "model_url": `https://huggingface.co/mlc-ai/${primaryModelId}`,
             "model_id": primaryModelId
         },
         {
